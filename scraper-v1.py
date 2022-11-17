979d8bd476f98d5837800fb20f24d8b00cd6d8b0
Galahallt <paolo_edni_v_espiritu@dlsu.edu.ph>
2022-11-18 01:18:39 +0800

    latest update

---
+++ scraper-v1.py
@@ -0,0 +1,126 @@
+""" Parallel Programming Project - Email Address Web Scraper
+In this project, the task is to create a web scraper that will collect 
+e-mail addresses that could be found from https://www.dlsu.edu.ph.
+"""
+
+__authors__ = ["Paolo Espiritu", "Jason Jabanes"]
+__email__ = ["paolo_edni_v_espiritu@dlsu.edu.ph", "jason_jan_jabanes@dlsu.edu.ph"]
+__date__ = "2022/11/24"
+__license__ = "MIT"
+__version__ = "1.0"
+
+from bs4 import BeautifulSoup
+
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+
+from webdriver_manager.chrome import ChromeDriverManager
+
+import time
+
+if __name__ == "__main__":
+    start_time = time.time()
+
+    # take user input
+    base_url, scrape_time, num_threads = input().split()
+
+    # convert data type of time and num_threads from str to int
+    scrape_time = int(scrape_time)
+    num_threads = int(num_threads)
+
+    timeout = 15 * scrape_time  # timeout set to 60 * x (x = scrape_time)
+
+    # test
+    website = f"{base_url}/staff-directory"
+
+    # setup chrome driver
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+
+    # maximize screen
+    driver.maximize_window()
+
+    # Send GET request to the url
+    driver.get(website)
+
+    # accept cookies
+    WebDriverWait(driver, 10).until(
+        EC.element_to_be_clickable(
+            (
+                By.ID,
+                "cn-accept-cookie",
+            )
+        )
+    ).click()
+
+    # click on load more button until it is unavailable
+    while time.time() < start_time + timeout:
+        try:
+            WebDriverWait(driver, 30).until(
+                EC.element_to_be_clickable(
+                    (
+                        By.XPATH,
+                        '//button[text()="Load More..."]',
+                    )
+                )
+            ).click()
+        except Exception as e:
+            print(e)
+            break
+
+    # initialize BeautifulSoup
+    soup = BeautifulSoup(driver.page_source, "lxml")
+
+    # get personnel info of each div
+    personnel_list = soup.find_all(
+        "button", {"class": "dlsu-pvf-link-button btn btn-link"}
+    )
+
+    personnel_data = [["name", "email", "department"]]
+
+    # get id value of div to go to individual personnel page
+
+    for p in personnel_list:
+        personnel_id = str(p.get("value")).strip()
+        personnel_page = (
+            f"https://www.dlsu.edu.ph/staff-directory/?personnel={personnel_id}"
+        )
+
+        # get page of the personnel
+        driver.get(personnel_page)
+
+        # initialize soup with the new page source
+        soup = BeautifulSoup(driver.page_source, "lxml")
+
+        soup.prettify()
+
+        # get department
+        ul = soup.find("ul", {"class": "list-unstyled text-capitalize text-center"})
+
+        for li in ul.find_all("li", {"class": False, "id": False}):
+            department = li.find("span")
+
+        # get name
+        name = soup.find("h3")
+
+        # get e-mail
+        email = soup.find("a", {"class": "btn btn-sm btn-block text-capitalize"})
+
+        if email:
+            email = email["href"].replace("mailto:", "")
+
+        # put it in a list
+        personnel_info = [name.get_text(), email, department.get_text()]
+
+        if personnel_info not in personnel_data:
+            personnel_data.append(personnel_info)
+
+    print(personnel_data)
+
+    with open("result.txt", "w") as file:
+        for row in personnel_data:
+            file.write(",".join([str(a) for a in row]) + "\n")
+
+    driver.quit()
